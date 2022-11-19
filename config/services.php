4116019cc1b9880a5fa1e8d1979ccc7ea84331a8
ashuchn <ashutoshchauhan129@gmail.com>
2022-11-19 22:14:19 +0530

    dynamic redirect url

--- config/services.php
+++ config/services.php
@@ -32,7 +32,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_SECRET'),
-        'redirect' => 'http://127.0.0.1:8000/callback/google'
+        'redirect' => url('/').'/callback/google'
     ]
 
 ];
