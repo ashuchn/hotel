0c694357b9e8ca82e8fde3f2a039d92a2f70f6e8
ashuchn <ashutoshchauhan129@gmail.com>
2022-11-19 22:17:53 +0530

    dynamic redirect url

--- config/services.php
+++ config/services.php
@@ -32,7 +32,8 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_SECRET'),
-        'redirect' => url('/').'/callback/google'
+        'redirect' => 'https://ashutosh-hotel.herokuapp.com/callback/google'
+        // 'redirect' => 'http://127.0.0.1:8000/callback/google'
     ]
 
 ];
